9ec9e377fb1d55f65960e8138022cc67f6256f18
Aaron Cronin <cronin@live.co.uk>
2013-03-14 19:55:47 +0000

    More work towards creation

--- digital_ocean_api.rb
+++ digital_ocean_api.rb
@@ -7,6 +7,8 @@ class DigitalOceanAPI
 
   BASE_URI = 'https://api.digitalocean.com'
   DROPLETS_URI = BASE_URI + '/droplets/'
+  SIZES_URI = BASE_URI + '/sizes/'
+  IMAGES_URI = BASE_URI + '/images/'
 
   MASTER_NAME = 'SuicuneNouveau'
 
@@ -14,9 +16,6 @@ class DigitalOceanAPI
     credentials = YAML.load_file(CONFIG)['creds']
     @client_key = credentials['client']
     @api_key = credentials['api']
-    puts "Loaded creds..."
-    puts "Found _this_ droplet: " << master_droplet.inspect
-
   end
 
   def auth_params
@@ -31,4 +30,24 @@ class DigitalOceanAPI
   def master_droplet
     get_droplets.select { |droplet| droplet["name"] == MASTER_NAME }[0]
   end
+
+  def sizes
+    response = HTTParty.get(SIZES_URI + "?#{auth_params}")
+    possible_sizes = JSON.parse(response.body)["sizes"]
+  end
+
+  def images
+    response = HTTParty.get(IMAGES_URI + "?#{auth_params}")
+    possible_sizes = JSON.parse(response.body)["images"]
+  end
+
+  def create_slave
+    config = YAML.load_file(CONFIG)['slave']
+    params = "/new?"
+    params << "name=#{config['name']}"
+    params << "&size_id=#{config['size_id']}"
+    params << "&image_id=#{config['image_id']
+    response = HTTParty.get(DROPLETS_URI + params + "&#{auth_params}")
+  end
+
 end
--- main.rb
+++ main.rb
@@ -1,5 +1,34 @@
 #!/usr/bin/env ruby
 
+require 'gli'
+include GLI::App
+
 require './digital_ocean_api.rb'
 
 DO = DigitalOceanAPI.new
+
+command :this do |c|
+  c.action do
+    puts "This droplet: #{DO.master_droplet}"
+  end
+end
+
+command :sizes do |c|
+  c.action do
+    puts "Available sizes: #{DO.sizes}"
+  end
+end
+
+command :images do |c|
+  c.action do
+    puts "Available images: #{DO.images}"
+  end
+end
+
+command  :deploy do |c|
+  c.action do
+    DO.create_slave
+  end
+end
+
+exit run(ARGV)
